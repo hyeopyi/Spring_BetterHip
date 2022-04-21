bbfff5d5b399c95d72969a06750c3de93ffb7f61
Sunggyu Im <hyeopyi@gmail.com>
2022-04-21 09:47:09 +0900

    [update] 2022/04/21 Blob 테스트 진행 중

--- src/main/java/com/team1/betterhip/command/CartListCommand.java
+++ src/main/java/com/team1/betterhip/command/CartListCommand.java
@@ -3,6 +3,7 @@ package com.team1.betterhip.command;
 
 
 
+import java.sql.Blob;
 import java.util.ArrayList;
 import java.util.Base64;
 
@@ -12,6 +13,7 @@ import org.apache.ibatis.session.SqlSession;
 
 import org.springframework.ui.Model;
 import com.team1.betterhip.dao.CartDao;
+import com.team1.betterhip.dao.TestDao;
 import com.team1.betterhip.dto.CartDto;
 
 public class CartListCommand implements BetterHipCommand {
@@ -21,23 +23,30 @@ public class CartListCommand implements BetterHipCommand {
 		
 		
 
-		HttpSession session = request.getSession();
+		
+		
+		
+		TestDao dao = sqlSession.getMapper(TestDao.class);
+		Blob img = dao.blobTestDao("1");
+		System.out.println("dddddd");
+		
+
 	
 
-		CartDao dao = sqlSession.getMapper(CartDao.class);		
-	
-		ArrayList<CartDto> dtos = dao.CartListDao(session.getAttribute("USER_ID").toString());
-		ArrayList<byte[]> dtosB = dao.CartListImgDao(session.getAttribute("USER_ID").toString());
-
-
-
-		byte[] img = null;
-		for(int i=0; i<dtosB.size(); i++) {
-			img = Base64.getEncoder().encode(dtosB.get(i));
-		}
-
-		model.addAttribute("CartList", dtos);
-		model.addAttribute("Img", img);
+//		CartDao dao = sqlSession.getMapper(CartDao.class);		
+//	
+//		ArrayList<CartDto> dtos = dao.CartListDao(session.getAttribute("USER_ID").toString());
+//		ArrayList<byte[]> dtosB = dao.CartListImgDao(session.getAttribute("USER_ID").toString());
+//
+//
+//
+//		byte[] img = null;
+//		for(int i=0; i<dtosB.size(); i++) {
+//			img = Base64.getEncoder().encode(dtosB.get(i));
+//		}
+//
+//		model.addAttribute("CartList", dtos);
+//		model.addAttribute("Img", img);
 
 	}
 
---
+++ src/main/java/com/team1/betterhip/dao/TestDao.java
@@ -0,0 +1,9 @@
+package com.team1.betterhip.dao;
+
+import java.sql.Blob;
+
+public interface TestDao {
+	
+	public Blob blobTestDao(String id);
+
+}
